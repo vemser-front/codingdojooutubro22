f0c434a2aed7743e6f41c2d7cf19331823e55f86
Eduardo <eduhmq@gmail.com>
2022-10-21 16:06:01 -0300

    criado map linhas da tabela

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
 let exibir = document.querySelector('.nomeUser');
 
-function adicionaUser(){
-  const users = [
-    { nome: 'Mr Marta Silva', email: 'marta@teste.com', role: 'User' },
-    { nome: 'Mr Amanda Soares', email: 'amanda.soares@teste.com', role: 'Admin' },
-    { nome: 'Mr Paula Silva', email: 'paula.silva@teste.com', role: 'User' },
-    { nome: 'Mr Maria Jung', email: 'cristijung@yahoo.com.br', role: 'User' }
+function adicionaUser() {
+  const users = [{
+      nome: 'Mr Marta Silva',
+      email: 'marta@teste.com',
+      role: 'User'
+    },
+    {
+      nome: 'Mr Amanda Soares',
+      email: 'amanda.soares@teste.com',
+      role: 'Admin'
+    },
+    {
+      nome: 'Mr Paula Silva',
+      email: 'paula.silva@teste.com',
+      role: 'User'
+    },
+    {
+      nome: 'Mr Maria Jung',
+      email: 'cristijung@yahoo.com.br',
+      role: 'User'
+    }
   ];
 
-  let teste = users.map((user) => exibir.innerHTML = user.nome);
-  console.log(teste)
+  users.map((user) => exibir.innerHTML += `<tr><td>${user.nome}</td><td>${user.email}</td><td>${user.role}</td></tr>`);
+  console.log(exibir)
+
+
+
 };
