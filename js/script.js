b24dba76e7077f8b26c11ae98f121615ee27484a
Danieljacon <danielcamillo2020@gmail.com>
2022-10-21 16:46:23 -0300

    Tentativa de adicionar os dados

--- js/script.js
+++ js/script.js
@@ -1,38 +1,66 @@
-let exibir = document.querySelector('.nomeUser');
+let exibir = document.querySelector(".nomeUser");
+const adicionar = document.getElementById("adicionar");
+const nome = document.getElementById("nome");
+const email = document.getElementById("email");
+const funcao = document.getElementById("funcao");
 
-function adicionaUser() {
-  const users = [{
-      nome: 'Mr Marta Silva',
-      email: 'marta@teste.com',
-      role: 'User'
-    },
-    {
-      nome: 'Mr Amanda Soares',
-      email: 'amanda.soares@teste.com',
-      role: 'Admin'
-    },
-    {
-      nome: 'Mr Paula Silva',
-      email: 'paula.silva@teste.com',
-      role: 'User'
-    },
-    {
-      nome: 'Mr Maria Jung',
-      email: 'cristijung@yahoo.com.br',
-      role: 'User'
-    }
-  ];
+const users = [
+  {
+    nome: "Mr Marta Silva",
+    email: "marta@teste.com",
+    role: "User",
+  },
+  {
+    nome: "Mr Amanda Soares",
+    email: "amanda.soares@teste.com",
+    role: "Admin",
+  },
+  {
+    nome: "Mr Paula Silva",
+    email: "paula.silva@teste.com",
+    role: "User",
+  },
+  {
+    nome: "Mr Maria Jung",
+    email: "cristijung@yahoo.com.br",
+    role: "User",
+  },
+];
 
-  users.map((user) => exibir.innerHTML += `<tr><td>${user.nome}</td><td>${user.email}</td><td>${user.role}</td> <td><button class ="btn btn-primary" onclick ="editarNome(this)">Editar</button> <button class ="btn btn-danger" onclick="deletar(this.parentNode)">Deletar</button></td></tr>`);
-  console.log(exibir)
+users.map(
+  (user) =>
+    (exibir.innerHTML += `
+    <tr>
+      <td>${user.nome}</td>
+      <td>${user.email}</td>
+      <td>${user.role}</td>
+      <td>
+        <button class ="btn btn-primary" onclick ="editarNome(this)">Editar</button>
+        <button class ="btn btn-danger" onclick="deletar(this.parentNode)">Deletar</button>
+      </td>
+    </tr>`)
+);
 
-};
+adicionar.addEventListener("click", (e) => {
+  e.preventDefault();
+  users.push({
+    nome: nome.value,
+    email: email.value,
+    role: funcao.value,
+  });
+  console.log(users);
+  // console.log(nome.value)
+});
 
-function deletar(e) {
-  e.parentElement.remove()
-}
 
-function editarNome(e) {
-  e.parentElement[0].innerText = prompt("Digite um novo nome: ")
-}
+console.log(exibir);
+
+
+// function deletar(e) {
+//   e.parentElement.remove();
+// }
+
+// function editarNome(e) {
+//   e.parentElement[0].innerText = prompt("Digite um novo nome: ");
+// }
 
